bf99a78554aeacb6cab9940b12ef3367b4992b3c
Osokin Yaroslav <osyaros.2005@yandex.ru>
2024-11-17 09:57:33 +0300

    fix force

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,8 +4,6 @@
 <script setup lang="ts">
 import './styles/global.scss';
 import './styles/variables.scss';
-import '@gravity-ui/uikit/styles/fonts.css';
-import '@gravity-ui/uikit/styles/styles.css';
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { getCookie } from './utils/getCookie';
