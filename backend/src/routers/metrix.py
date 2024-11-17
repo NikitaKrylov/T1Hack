62eac94239e504cbfcea5d375ecf42925ec6239c
NikitaKrylov <idoverchiviiloh@gmail.com>
2024-11-17 13:58:04 +0300

    ref: change metrics endpoint to post

--- backend/src/routers/metrix.py
+++ backend/src/routers/metrix.py
@@ -1,6 +1,6 @@
 from datetime import date
 from datetime import datetime
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Body
 from src.services import metrics as service
 from src.database.db import get_async_session
 from fastapi_cache.decorator import cache
@@ -10,9 +10,9 @@ router = APIRouter(
     prefix="/metrix",
 )
 
-@router.get("")
+@router.post("")
 # @cache(60)
-async def get_all_metrix(sprint_id: int, first_date: date, second_date: date,  session=Depends(get_async_session)):
+async def get_all_metrix(sprint_id: int = Body(), first_date: date = Body(), second_date: date = Body(),  session=Depends(get_async_session)):
     start = datetime.now()
     result = await service.get_sprint_metrics(session=session, sprint_id=sprint_id, first_date=first_date, second_date=second_date)
     end = datetime.now()
