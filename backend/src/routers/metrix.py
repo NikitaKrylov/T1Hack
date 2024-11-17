d02ccd93ebd76fb60fb5ea7ac2ff59963b134a53
NikitaKrylov <idoverchiviiloh@gmail.com>
2024-11-17 12:31:03 +0300

    feat: add common metrics

--- backend/src/routers/metrix.py
+++ backend/src/routers/metrix.py
@@ -14,12 +14,8 @@ router = APIRouter(
 # @cache(60)
 async def get_all_metrix(sprint_id: int, first_date: date, second_date: date,  session=Depends(get_async_session)):
     start = datetime.now()
-    result = await service.get_blockedtasksCHD_metricx(session=session, sprint_id=sprint_id, first_date=first_date, second_date=second_date)
+    result = await service.get_sprint_metrics(session=session, sprint_id=sprint_id, first_date=first_date, second_date=second_date)
     end = datetime.now()
     print(f'All time: {end - start}')
     return result
 
-
-@router.get("/kvipolneniyu", summary='К выполнению', response_model=int)
-async def get_kvipolneniyu_metric(sprint_id: int, first_date: date, second_date: date,  session=Depends(get_async_session)):
-    pass
